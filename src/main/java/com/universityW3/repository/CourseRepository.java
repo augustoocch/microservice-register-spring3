20452b04a5ba2a99c2e9b7e5d9393c45a78a20fa
augustoocch <augusto.occh@gmail.com>
2023-05-07 17:10:43 -0300

    Deleted entities to build specific microservice

--- src/main/java/com/universityW3/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.universityW3.repository;
-
-import com.universityW3.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
--- src/main/java/com/universityW3/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.universityW3.repository;
-
-import com.universityW3.model.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Orders, Long> {
-}
