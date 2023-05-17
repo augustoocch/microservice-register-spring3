acd827ecc2ea851a03ddd37b9482de9892958616
augustoocch <augusto.occh@gmail.com>
2023-05-17 17:44:26 -0300

    Register returns token successfully 03

--- src/main/java/com/intralink/user/micro/ApplicationIntraLinkUserMicro.java
+++ src/main/java/com/intralink/user/micro/ApplicationIntraLinkUserMicro.java
@@ -10,10 +10,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
-//@ComponentScan(basePackageClasses = {SecurityConfig.class, UserService.class, JwtTokenFilter.class})
-//{"com.intralink.user.micro.config", "com.intralink.user.micro.repository"})
 @ComponentScan(basePackages={"com.intralink.user.micro.config", "com.intralink.user.micro.controller", "com.intralink.user.micro.model","com.intralink.user.micro.repository", "com.intralink.user.micro.security","com.intralink.user.micro.security.control", "com.intralink.user.micro.service" })
-//@ComponentScan({"com.intralink.user.micro.repository"})
 public class ApplicationIntraLinkUserMicro {
 
     public static void main(String[] args) {
